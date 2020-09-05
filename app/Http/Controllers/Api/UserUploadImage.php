7fc1e4e390acd1edac1bc20c86e978e60fb67950
BaoNguyen <17130010@st.hcmuaf.edu.vn>
2020-09-05 21:51:02 +0700

    add upload api image

--- app/Http/Controllers/Api/UserUploadImage.php
+++ app/Http/Controllers/Api/UserUploadImage.php
@@ -4,11 +4,24 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserUploadImage extends Controller
 {
     //
-    public function upload(Request $request){
+    public function storeImage(Request $request)
+    {
+        $files = $request->file('image');
+        $folder = public_path('../public/storage/' . Auth::user()->studentid . '/');
 
+        if (!Storage::exists($folder)) {
+            Storage::makeDirectory($folder, 0775, true, true);
+        }
+
+        if (!empty($files)) {
+            foreach ($files as $file) {
+                Storage::disk(['drivers' => 'local', 'root' => $folder])->put($file->getClientOriginalName(), file_get_contents($file));
+            }
+        }
     }
 }
